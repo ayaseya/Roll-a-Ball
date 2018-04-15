bcf80cc95d47b58fc0be546a416b09fb031c3dbd
ayaseya <ayaseya@gmail.com>
2018-04-15 16:33:39 +0900

    Created a new tutorial project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player; 	// 操作対象のボール
+
+	private Vector3 offset;		// カメラからボールまでの距離、
+								// カメラは常にボールからoffset分の距離を保つことになる
+
+	void Start ()
+	{
+		// ボールの初期位置からoffsetを初期化する
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		// （移動後の）ボールの位置を元にカメラの位置を移動する
+		transform.position = player.transform.position + offset;		
+	}
+}
